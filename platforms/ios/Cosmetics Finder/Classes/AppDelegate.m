96c26b4ca9d6f0bfeaa6ed109e9f156b7ff19813
Andy Johan <andyjohansson419@gmail.com>
2016-02-28 22:57:04 +0200

    Updates

--- platforms/ios/Cosmetics Finder/Classes/AppDelegate.m
+++ platforms/ios/Cosmetics Finder/Classes/AppDelegate.m
@@ -19,7 +19,7 @@
 
 //
 //  AppDelegate.m
-//  Findation
+//  Cosmetics Finder
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
@@ -92,7 +92,7 @@
 }
 
 // this happens while we are running ( in the background, or from within our own app )
-// only valid if Findation-Info.plist specifies a protocol to handle
+// only valid if Cosmetics Finder-Info.plist specifies a protocol to handle
 - (BOOL)application:(UIApplication*)application openURL:(NSURL*)url sourceApplication:(NSString*)sourceApplication annotation:(id)annotation
 {
     if (!url) {
--- platforms/ios/Cosmetics Finder/main.m
+++ platforms/ios/Cosmetics Finder/main.m
@@ -18,7 +18,7 @@
  */
 //
 //  main.m
-//  Findation
+//  Cosmetics Finder
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
